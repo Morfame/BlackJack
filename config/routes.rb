8d59550636a1c1a705dcdb59d8f0c68e7fb154da
Steve Morris <baramorf.com>
2023-02-23 09:46:28 +0000

    Basic screens added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root "cards#index"
+  resources :cards
+  #get 'cards/index'
+  #get 'cards/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
