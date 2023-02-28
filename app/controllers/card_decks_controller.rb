67580dfa5ea8533ccc82ec25fe6e1875653a6a8c
Steve Morris <baramorf.com>
2023-02-28 18:56:25 +0000

    In page processing

--- app/controllers/card_decks_controller.rb
+++ app/controllers/card_decks_controller.rb
@@ -5,6 +5,19 @@ class CardDecksController < ApplicationController
   def new
     vdeck = CardDeck.new
     @vdeck = vdeck.shuffle!
+    @dealer = User.new
+    @player = User.new
+    # Deals hand to Dealer
+    vTmp = @vdeck.draw!
+    @dealer.add(Card.new(vTmp.suit, vTmp.rank))
+    vTmp = @vdeck.draw!
+    @dealer.add(Card.new(vTmp.suit, vTmp.rank))
+    
+    # Deals hand to Player
+    vTmp = @vdeck.draw!
+    @player.add(Card.new(vTmp.suit, vTmp.rank))
+    vTmp = @vdeck.draw!
+    @player.add(Card.new(vTmp.suit, vTmp.rank))
   end
 
   def show
